f3b4219282c5eaed4a6b32b608015c3002651f03
Fandahao <fandahao17@mail.ustc.edu.cn>
2020-05-10 18:30:11 +0800

    Simplify feature selection

--- svd/featureSelection.m
+++ svd/featureSelection.m
@@ -4,29 +4,12 @@ function [features, selected] = featureSelection(A, k, r)
     [~, ~, V] = svd(A);
     Vk = V(:, 1:k);
     
-    
     % Compute the normalized leverage scores
     p = vecnorm(Vk') .^ 2 / k;
 
     % r i.i.d sample
     features = sort(datasample(1:d, r, 'Replace', false, 'Weights', p));
-    p(features)
-    % Sampling matrix
-%     S = zeros(d, r);
-%     for i = 1 : r
-%         S(features(i), i) = 1;
-%     end
-%     size(S);
-    % Rescaling matrix
-%     D = zeros(r, r);
-%     for i = 1 : r
-%         D(i, i) = 1 / sqrt(r * p(features(i)));
-%     end
 
-    % According to Section 4.2
-%     selected = A * S * D;
-    % [A(1, 1:100); selected(1, 1:100)]
-    
     % Select
     selected = A(:, features);
     % Rescale
