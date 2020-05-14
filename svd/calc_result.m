45203afab3157a0115ea562f406f2db2e577b19f
Fandahao <fandahao17@mail.ustc.edu.cn>
2020-05-14 14:53:11 +0800

    Repeat multiple times to get good result

---
+++ svd/calc_result.m
@@ -0,0 +1,29 @@
+function [P,F] = calc_result(A, clusters, label, num_clusters)
+    % Calculate F
+    num = [sum(clusters == 1) sum(clusters == 2) sum(clusters == 3)];
+    
+    [rows, ~] = size(A);
+    X = zeros(rows, num_clusters);
+    
+    for i=1 : rows
+        X(i, clusters(i)) = 1 / sqrt(num(clusters(i)));
+    end
+    
+    F = norm(A - X * X' * A) / norm(A);
+    
+    % Calculate P
+    orders = [1 2 3; 1 3 2; 2 1 3; 2 3 1; 3 1 2; 3 2 1];
+    p1 = label == 1;
+    p2 = label == 2;
+    p3 = label == 3;
+    P = 0;
+    for i= 1:length(orders)
+        order = orders(i, :);
+        curp = sum(clusters(p1) == order(1)) + sum(clusters(p2) == order(2)) + sum(clusters(p3) == order(3));
+        curp = curp / 184;
+        if curp > P
+            P = curp;
+        end
+    end       
+end
+
--- svd/dokmeans.m
+++ svd/dokmeans.m
@@ -4,6 +4,16 @@ k = 3; r = 10 * k;
 
 [label, data, words] = prep();
 
-[features, selectedMatrix] = featureSelection(data, k, r);
-[res, C] = kmeans(selectedMatrix, k, 'Replicates', 30, 'Maxiter', 30);
-res'
+bestp = 0;
+ps = [];
+for i= 1:50
+    [features, selectedMatrix] = featureSelection(data, k, r);
+    [res, C] = kmeans(selectedMatrix, k, 'Replicates', 10, 'Maxiter', 30);
+
+    [P, F] = calc_result(data, res', label, k);
+    if P > bestp
+        bestp = P;
+    end
+    ps = [ps P];
+end
+bestp
