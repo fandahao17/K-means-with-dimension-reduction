04cfac2211089f92f19feb3d3ab52179a523fc93
Fandahao <fandahao17@mail.ustc.edu.cn>
2020-05-18 21:59:54 +0800

    Add some test results

--- svd/runorl.m
+++ svd/runorl.m
@@ -8,12 +8,15 @@ start = samples * (0:k-1) + 1;
 % bestp = 0;
 % bestf = 1;
 ps = [];
+ts = [];
 for r= 5:5:300
+    tic
     [features, selectedMatrix] = featureSelection(data, k, r);
     S = selectedMatrix(start, :);
     S = repmat(S, 1, 1, 30);
 
     [res, C] = kmeans(selectedMatrix, k, 'Distance', 'sqEuclidean', 'Replicates', 30, 'Maxiter', 30, 'start', S);
+    ts = [ts toc];
     P = sum(res' == label) / (k * samples);
     ps = [ps P];
     r
